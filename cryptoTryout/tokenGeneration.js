43d71474230c03a3ea7f68a182387c7bbb8ff790
Ubuntu <ubuntu@ip-172-31-18-151.us-west-2.compute.internal>
2019-11-22 05:26:04 +0000

    Changes

--- cryptoTryout/tokenGeneration.js
+++ cryptoTryout/tokenGeneration.js
@@ -1,47 +1,58 @@
-const randomstring = require("randomstring");
-const crypto = require('crypto');
-const uuid = require('uuid/v1');
-const secret = 'My guy';
-const url = 'lacleanupsites.com';
-const tempUserName = 'Flo';
-const API_PORT = '6969';
 const express = require('express');
+const jwt = require('jwt-simple');
+const API_PORT =  4002;
+const uuid = ('uuid/v1');
+const app = express();
+const bodyParser = require('body-parser');
 
+var cors = require('cors');
+app.use (cors());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
-//console.log(randomstring.generate(8));
+let secret = 'lemons';
+let timeNow = new Date();
 
-//console.log(uuid());
-//console.log(uuid('hi'));
-//console.log(uuid(randomstring.generate(4)));
-//for (i = 0; i < 20; i++){
-//	console.log(uuid());
-//}
-
-let tempUuid = uuid();
-console.log(tempUuid);
-let login_token = uuid() + parseInt(new Date().getTime()).toString(36);
-console.log(login_token);
-const hashed_token = crypto.createHash('sha512').update(login_token).digest('base64');
-console.log(hashed_token);
-
-let tempTimeStamp = new Date();
-
-const token_object = {
-	whew: tempTimeStamp),
-	expires: new Date(tempTimeStap.getHours() + 1),
-	'hashedToken':hashed_token,
+let payload = { created: new Date(),
+		expires: new Date(timeNow.getHours() + 1),
+		user: 'Matt',
 };
 
-
-
-
-/*
-let login_token = uuid().
-
-const hashed_token = crypto.createHash('sha512',secret).update(login_token).digest('base64');
-
-const token_object = {
-	'when':new Date(),
-	'hashedToken': hashed_token,
+var myLogger = function (req, res, next){
+	console.log('LOGGED')
+	next()
 };
-*/
+
+app.use(myLogger);
+
+var requestTime = function (req, res, next) {
+	req.requestTime = Date.now()
+	next()
+};
+
+app.use(requestTime);
+
+const token = jwt.encode(payload,secret);
+
+app.get('/', function (req, res){
+	res.send('Hello World');
+});
+
+app.get('/getToken', function (req, res) {
+	var responseText = 'Hello World!<br>'+ req.requestTime ;
+	res.json(token);
+});
+
+
+app.get('/checkToken', function (req, res) {
+	var token = req.headers['x-access-token'];
+	if (!token) return res.status(401).send({auth: false, message: 'No token provided.'});
+	jwt.decode(token, secret, function (err, decoded) {
+		if (err) return res.status(500).send({auth: false, message: 'Failed to authenticate token.' });
+		res.status(200).send(decoded);
+	});
+});
+
+
+app.listen(API_PORT, () => console.log(`App  listening on port ${API_PORT}`));
+
